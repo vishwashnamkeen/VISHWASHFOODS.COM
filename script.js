bd86167d0213bc86e70348574e2cb172c4215ea3
vishwashnamkeen <vishwashnamkeen@gmail.com>
2026-08-17 10:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,37 +3,63 @@ const products = [
     {
         id: 1,
         name: "Black Pepper Banana Wafers",
-        price: 120,
-        oldPrice: 150,
-        img: "BLACKPEPPERBANANAWAFERS.png",
+        price: 80,
+        oldPrice: 120,
+        img: "blackpeparbananachips.png",
         rating: "★★★★★ (4.9)"
     },
     {
         id: 2,
         name: "Pani Puri Flavour Banana Wafers",
-        price: 130,
-        oldPrice: 160,
-        img: "PANIPURIFLAVOURBANANAWEFERS.png",
+        price: 80,
+        oldPrice: 110,
+        img: "panipuribananachips.png",
         rating: "★★★★★ (5.0)"
     },
     {
         id: 3,
-        name: "Salted Banana Wafers",
-        price: 110,
-        oldPrice: 140,
-        img: "SALTEDBANANAWEFERS.png",
+        name: "Rajshthani Aloo Bhujiya ",
+        price: 80,
+        oldPrice: 110,
+        img: "aloobhujiya.png",
         rating: "★★★★☆ (4.8)"
     },
     {
         id: 4,
-        name: "Spicy Banana Wafers",
-        price: 125,
-        oldPrice: 155,
-        img: "SPICYBANANAWAFERS.png",
+        name: "fast special banana chips",
+        price: 80,
+        oldPrice: 110,
+        img: "fastbananachips.png",
         rating: "★★★★★ (4.9)"
     }
 ];
+         id: 5,
+        name: "Gujarati Namkeen",
+        price: 80,
+        oldPrice: 110,
+        img: "gujaratinamkeen.png",
+        rating: "★★★★★ (4.9)"
 
+         id: 6,
+        name: "Jeera Puri",
+        price: 80,
+        oldPrice: 110,
+        img: "jeerapuri.png",
+        rating: "★★★★★ (4.9)"
+        
+         id: 7,
+        name: "Mitha Namkeen",
+        price: 80,
+        oldPrice: 110,
+        img: "mithanamkeen.png",
+        rating: "★★★★★ (4.9)"
+
+         id: 8,
+        name: "Peri Peri Banana Chips",
+        price: 80,
+        oldPrice: 110,
+        img: "periperibananachips.png",
+        rating: "★★★★★ (4.9)"
 let cart = [];
 let wishlist = [];
 
